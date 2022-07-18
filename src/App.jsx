146d6e3b884c2e3cb077b63417a8be053ad8b13c
im-ashish00 <iamashishsharma00@gmail.com>
2022-07-18 11:37:04 +0530

    Add Modal component and basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import './App.css';
 
 export default function App() {
-  return null;
+  const FIRST_BOX = {
+    position: 'relative',
+    zIndex: 1,
+  };
+  const OUTER_BOX_ON_TOP = {
+    zIndex: 2,
+    position: 'relative',
+  };
+
+  return (
+    <>
+      <div className="first" style={FIRST_BOX}>
+        <button>Open modal</button>
+      </div>
+
+      <div className="second" style={OUTER_BOX_ON_TOP}>
+        OUTER CONTENT
+      </div>
+    </>
+  );
 }
